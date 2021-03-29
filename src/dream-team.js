2edf9a6d60bc3b36878fc82cc796dad614c16ce7
Ekaterin22 <77670720+Ekaterin22@users.noreply.github.com>
2021-03-30 01:00:07 +0400

    feat: implement dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  letters = []
+  // если неправильные входные данные (не массив)
+  if(!Array.isArray(members)) return false;
+  members.forEach( elem => {
+    // работаем, только если текущий элемент массива - строка
+    if(typeof elem == 'string'){
+      for(i = 0; i < elem.length; i++){
+        // игнорируем пробелы
+          if(elem[i] == ' ') continue;
+          else{
+            //если не "пробел" (значит начало имени) - добавить первую букву в массив
+            letters.push(elem[i])
+            break;
+          }
+        }
+    }
+  })
+  //полученный массив объединим в строку, переведем в "капс", разобьем для сортировки и снова соединим в строку
+  return letters.join('').toUpperCase().split('').sort().join('')
 };
+
